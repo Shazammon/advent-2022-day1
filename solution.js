eb60d3f507259d91c48b2511992a75c011192437
Shazammon <ammonorgill@gmail.com>
2022-12-11 15:22:07 -0700

    code clean up

--- solution.js
+++ solution.js
@@ -1,27 +1,9 @@
 const fs = require('fs')
 const { parse } = require('path')
-// let datastring = ''
 
-// fileconverter = fs.readFile('data.js', (err, data) => {
-//     if (err) throw err;
-//     // Number(data)
-//    datastring = data.toString()
-//    console.log(datastring)
-//     // console.log(data);
-//     return datastring
-// })
-
-
-// fileconverter()
-// console.log(datastring)
 
 let data = fs.readFileSync('data.txt', 'utf-8')
-// console.log(data)
 
-// console.log(typeof(data))
-
-// let separateLines = data.split(/[^\r\n]+/g);
-// console.log(separateLines.length)
 const stringData = data.split(/\r?\n/)
 const numData = stringData.map(function(str) {
     if (str === '') {
