dc37f8cf4cec4dd6fda8683fe82fc3ac3b9b9503
Shazammon <ammonorgill@gmail.com>
2022-12-11 14:20:03 -0700

    working on datastring

--- solution.js
+++ solution.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
+// let datastring = ''
 
-fs.readFile('data.js', 'utf-8', (err, data) => {
+fs.readFile('data.js', (err, data) => {
     if (err) throw err;
-    Number(data)
-    console.log(data);
+    // Number(data)
+   let datastring = data.toString()
+//    console.log(datastring)
+    // console.log(data);
+    return datastring
 })
 
-// console.log(data)
+console.log(datastring)
